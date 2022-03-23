939ea3ee292a5bde3edd051d8b5d7fcbd38c5cfc
ilyaKineev <kineev.ilya@gmail.com>
2022-03-23 19:09:34 +0300

    New project

---
+++ src/main/java/com/ZOOManager/ZOOManager/Controller.java
@@ -0,0 +1,13 @@
+package com.ZOOManager.ZOOManager;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping(value = "/")
+    public String home(){
+        return "Hello";
+    }
+}
---
+++ src/main/java/com/ZOOManager/ZOOManager/ZooManagerApplication.java
@@ -0,0 +1,11 @@
+package com.ZOOManager.ZOOManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooManagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ZooManagerApplication.class, args);
+	}
+}
